e5d80da49ac57e007e1e7312684d4ef37b632e3f
Sachin Prasad <58424072+sachinprasad@users.noreply.github.com>
2023-10-30 04:49:04 +0530

    Create check_digit_generator.rb

---
+++ check_digit_generator.rb
@@ -0,0 +1,17 @@
+class CheckDigitGenerator
+  # Returns barcode along with Check digit 
+  def barcode(bar_code)
+    "#{bar_code}#{isbn13_check_digit(bar_code)}"
+  end
+  # Returns on check digit 
+  def isbn13_check_digit(bar_code)
+    int_chars = bar_code.to_s.chars.map(&:to_i)
+    marker = 1
+    sum = 0
+    int_chars.each do |num|
+      sum += num*marker
+      marker = (marker == 1) ? 3 : 1
+    end
+    (10 - sum % 10)
+  end
+end
