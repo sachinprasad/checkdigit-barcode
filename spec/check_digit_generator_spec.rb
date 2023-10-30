2e0a02a9db67efcc6b64ede830e46c0ef9c5880c
sachin prasad <sprasad1234@gmail.com>
2023-10-30 17:23:50 +0530

    - added spec wrapper

--- spec/check_digit_generator_spec.rb
+++ spec/check_digit_generator_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 require_relative '../check_digit_generator'
 
-describe '#barcode' do
-  it 'should return check digit appended to 978014300723' do
-    expect(CheckDigitGenerator.new.barcode(978014300723)).to eq "9780143007234"
-  end
-end
+describe CheckDigitGenerator do
 
-describe '#isbn13_check_digit' do
-  it 'should should return check digit of 978014300723' do
-    expect(CheckDigitGenerator.new.isbn13_check_digit(978014300723)).to eq 4
+  describe '#barcode' do
+    it 'should return check digit appended to 978014300723' do
+      expect(CheckDigitGenerator.new.barcode(978014300723)).to eq "9780143007234"
+    end
+  end
+
+  describe '#isbn13_check_digit' do
+    it 'should should return check digit of 978014300723' do
+      expect(CheckDigitGenerator.new.isbn13_check_digit(978014300723)).to eq 4
+    end
   end
 end
